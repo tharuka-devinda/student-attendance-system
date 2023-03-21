6fb132f39729db3ed4d4806d9d936b4f9be4c585
tharuka-devinda <tharukadevinda96@gmail.com>
2023-03-22 00:17:30 +0530

    Third commit

--- src/main/java/lk/ijse/dep10/app/controller/StudentViewController.java
+++ src/main/java/lk/ijse/dep10/app/controller/StudentViewController.java
@@ -1,17 +1,22 @@
 package lk.ijse.dep10.app.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
+import lk.ijse.dep10.app.db.DBConnection;
+import lk.ijse.dep10.app.model.Student;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class StudentViewController {
 
@@ -34,7 +39,7 @@ public class StudentViewController {
     private ImageView imgPicture;
 
     @FXML
-    private TableView<?> tblStudent;
+    private TableView<Student> tblStudent;
 
     @FXML
     private TextField txtID;
@@ -44,6 +49,66 @@ public class StudentViewController {
 
     @FXML
     private TextField txtSearch;
+
+    public void initialize() {
+
+        tblStudent.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblStudent.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblStudent.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("picture"));
+
+        loadAllStudents();
+
+        btnDelete.setDisable(true);
+        tblStudent.getSelectionModel().selectedItemProperty().addListener((ov, prev, current) -> {
+            btnDelete.setDisable(current == null);
+            if (current == null) return;
+
+            txtID.setText(current.getId() + "");
+            txtName.setText(current.getName());
+
+            if (current.getPicture() != null) {
+                try {
+                    InputStream is = current.getPicture().getBinaryStream();
+                    Image itemImage = new Image(is);
+                    imgPicture.setImage(itemImage);
+                    btnClear.setDisable(false);
+                } catch (SQLException e) {
+                    new Alert(Alert.AlertType.ERROR, "Failed to load the student's picture. Try again!").showAndWait();
+                    throw new RuntimeException(e);
+                }
+            } else {
+                btnClear.fire();
+            }
+        });
+
+        Platform.runLater(btnNewStudent::fire);
+    }
+
+    private void loadAllStudents() {
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM Student");
+            PreparedStatement stm2 = connection.prepareStatement("SELECT * FROM Picture WHERE student_id = ?");
+
+            while (rst.next()) {
+                String id = rst.getString("id");
+                String name = rst.getString("name");
+                Blob picture = null;
+
+                stm2.setString(1, id);
+                ResultSet rstPicture = stm2.executeQuery();
+                if (rstPicture.next()) {
+                    picture = rstPicture.getBlob("picture");
+                }
+                Student student = new Student(id, name, picture);
+                tblStudent.getItems().add(student);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to load customer details. Try again!", ButtonType.OK).show();
+        }
+    }
 
     @FXML
     void btnBrowseOnAction(ActionEvent event) throws MalformedURLException {
@@ -60,7 +125,9 @@ public class StudentViewController {
 
     @FXML
     void btnClearOnAction(ActionEvent event) {
-
+        Image image = new Image("/images/no-profile.png");
+        imgPicture.setImage(image);
+        btnClear.setDisable(true);
     }
 
     @FXML
